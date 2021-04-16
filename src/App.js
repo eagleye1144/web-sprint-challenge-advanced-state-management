4d060418d44f204494b432546b6f041cb6cf81ce
Mitchell Walker <eagleye1144@gmail.com>
2021-04-16 13:13:02 -0600

    App.js done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import { fetchSmurfs } from './actions/index';
+import { connect } from 'react-redux';
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
@@ -8,6 +9,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
+
+  componentDidMount() {
+    const { fetchSmurfs } = this.props;
+    console.log('component mounting!')
+    fetchSmurfs()
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -21,8 +30,9 @@ class App extends Component {
     );
   }
 }
+const mapDispatchToProps = {fetchSmurfs}
 
-export default App;
+export default connect(null, mapDispatchToProps)(App)
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
