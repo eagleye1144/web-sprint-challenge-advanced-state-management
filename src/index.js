b0d95d44f1c0f4cfb9df660f322e0c01678ed6ba
Mitchell Walker <eagleye1144@gmail.com>
2021-04-16 13:14:33 -0600

    fixed index.js bugs

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./App";
+import reducer from './reducers/index';
 
-const { worker } = require('./mocks/browser');
-worker.start();
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+
+
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-    <App />, 
-    rootElement
-);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
 
 //Task List:
 //1. Add in all necessary components and libary methods.
